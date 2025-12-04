f6573177bcd51f0881d44c9b05fc82b7e7d89a20
divyabhatt2004-cloud <divyabhatt2004@gmail.com>
2025-12-04 17:49:23 +0530

    Create Nav.php

---
+++ Nav.php
@@ -0,0 +1,81 @@
+ <nav class="navbar navbar-expand-lg p-0 bg-dark navbar-light d-none d-lg-block" id="top">
+     <div class="container">
+         <div class="row">
+             <span class="text-light fs-6 fw-lighter d-flex align-items-center">
+                 <div class="d-flex align-items-center me-2">
+                     <span> <i class="fa-solid fa-envelope me-1"></i></span>
+                     <p class="h6 m-0 fw-lighter"> info@company.com</p>
+                 </div>
+                 <div class="d-flex align-items-center ms-2">
+                     <i class="fa-solid fa-phone me-1 "></i>
+                     <p class="h6 m-0 fw-lighter"> 010-020-0340</p>
+                 </div>
+             </span>
+         </div>
+         <div class="row">
+             <span class="text-end text-light fs-6 fw-lighter">
+                 <div class="col-12 d-flex ">
+                     <span class=" bg-dark text-light ps-1 pe-1 text-center me-2">
+                         <i class="fa-brands fa-facebook-f me-1"></i>
+                         <i class="fa-brands fa-instagram me-1"></i>
+                         <i class="fa-brands fa-twitter me-1"></i>
+                         <i class="fa-brands fa-linkedin-in me-1"></i>
+                     </span>
+                 </div>
+             </span>
+         </div>
+     </div>
+ </nav>
+ <nav class="navbar navbar-expand-lg shadow">
+     <div class="container">
+         <a class="navbar-brand fs-1 text-success fw-medium" href="#">Zay</a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+             aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+             <div class="mx-auto">
+                 <ul class="navbar-nav" id="headerNav">
+                     <li class="nav-item px-5">
+                         <a class="nav-link text-black" aria-current="page" href="Index.php">Home</a>
+                     </li>
+                     <li class="nav-item px-5">
+                         <a class="nav-link text-black" href="About.php">About</a>
+                     </li>
+                     <li class="nav-item px-5">
+                         <a class="nav-link text-black" href="Shop.php">Shop</a>
+                     </li>
+                     <li class="nav-item px-5">
+                         <a class="nav-link text-black" href="Contact.php">Contact</a>
+                     </li>
+                     <li class="nav-item px-5">
+                         <a class="nav-link text-black" href="Admin.php">Admin</a>
+                     </li>
+                 </ul>
+             </div>
+             <div class="d-flex my-2 mb-2 align-items-center">
+                 <span class="me-2 p-1">
+                     <i class="fa-solid fa-magnifying-glass"></i>
+                 </span>
+                 <span class="me-2 p-1">
+                     <i class="fa-sharp fa-solid fa-cart-arrow-down"></i>
+                 </span>
+            
+                 <span class="me-2 p-1">
+                     <div class="dropdown">
+                         <a class="btn btn-white dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                             <i class="fa-solid fa-user"></i>
+                         </a>
+                         <ul class="dropdown-menu">
+                             <li><a class="dropdown-item text-primary" href="#">
+                                     Login
+                                 </a></li>
+                             <li><a class="dropdown-item text-primary" href="#">Register</a></li>
+                         </ul>
+                     </div>
+                 </span>
+             </div>
+         </div>
+     </div>
+ </nav>
