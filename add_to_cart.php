69eb6b89224149c9df119fd3741f8d30aae303b6
Divya <divyabhatt@gmail.com>
2025-12-15 16:57:52 +0530

    Update add_to_cart.php

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,7 +1,7 @@
 <?php
 include('./connect.php');
 $id = $_GET['id'];
-//$user_quantity = $_GET['quantity'];
+$quantity = $_GET['quantity'] ?? 1;
 
 $Cart_Query = "SELECT * FROM `cart` WHERE `product_id` = $id";
 $Cart_Result = mysqli_query($conn, $Cart_Query);
@@ -9,7 +9,6 @@ $Cart_row = mysqli_fetch_assoc($Cart_Result);
 
 if (!mysqli_num_rows($Cart_Result)) {
 
-    $quantity = 1;
     $productCart_query = "SELECT * FROM `product` WHERE `id`= $id";
     $productCart_result = mysqli_query($conn, $productCart_query);
     $productCart_row = mysqli_fetch_assoc($productCart_result);
@@ -18,18 +17,19 @@ if (!mysqli_num_rows($Cart_Result)) {
     $Cartproduct_name = $productCart_row['product name'];
     $Cartproduct_description = $productCart_row['description'];
     $Cartproduct_image = $productCart_row['image'];
-    $Cartproduct_quantity =  $quantity ?? $user_quantity ;
+    $Cartproduct_quantity = $quantity;
     $Cartproduct_price = $productCart_row['price'];
+    $Cartproduct_gst = $productCart_row['gst'];
 
 
-    $Cartdata_query = "INSERT INTO `cart`(`product_id`,`product name`, `description`, `image`, `quantity`, `price`)  VALUES ('$Cartproduct_id', '$Cartproduct_name','$Cartproduct_description','$Cartproduct_image','$Cartproduct_quantity','$Cartproduct_price')";
+    $Cartdata_query = "INSERT INTO `cart`(`product_id`,`product name`, `description`, `image`, `quantity`, `price`,`gst`)  VALUES ('$Cartproduct_id', '$Cartproduct_name','$Cartproduct_description','$Cartproduct_image','$Cartproduct_quantity','$Cartproduct_price','$Cartproduct_gst')";
     $Cartdata_result = mysqli_query($conn, $Cartdata_query);
 
     if ($Cartdata_result) {
         header('location:shop.php');
     }
 } else {
-        $Cart_row["quantity"] = $Cart_row["quantity"] + 1;
+        $Cart_row["quantity"] = $Cart_row["quantity"] + $quantity ;
         $cart_quantity = $Cart_row["quantity"];
         $Cart_update_query = "UPDATE `cart` SET `quantity`='$cart_quantity' WHERE `product_id`= $id";
         $Cart_update_Result = mysqli_query($conn, $Cart_update_query);
