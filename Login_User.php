020ea6bc6f11c3ea482aac86d9598544d328ec00
Divya <divyabhatt@gmail.com>
2025-12-16 17:13:48 +0530

    Update Login_User.php

--- Login_User.php
+++ Login_User.php
@@ -10,11 +10,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
         integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
 
 <body>
@@ -64,9 +65,12 @@
                                     </div>
                                     <div class="row justify-content-center mb-2">
                                         <div class="col-12">
-                                            <label for="user_password" class="form-label">Password</label>
-                                            <input type="password" class="form-control mb-3" name="user_password"
-                                                id="user_password" placeholder="Password" required>
+                                            <label for="loguser_password" class="form-label">Password</label>
+                                            <div class="input-group" id="log_password">
+                                                <input type="password" class="form-control" style="border-right: none ;" name="loguser_password" autocomplete="off"
+                                                    id="loguser_password" placeholder="Password" required>
+                                                <span class="p-2 input-group-text"><i class="fa-solid fa-eye"></i></span>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="row justify-content-center mb-2">
@@ -94,6 +98,21 @@
             </div>
         </div>
     </div>
+    <script>
+        $('#log_password i').on('click', function() {
+                $(this).toggleClass('fa-eye fa-eye-slash')
+                var x = document.getElementById("loguser_password");
+                if (x.type === "password") {
+                    x.type = "text";
+                } else {
+                    x.type = "password";
+                }
+            })
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+        crossorigin="anonymous"></script>
+
 </body>
 
 </html>
