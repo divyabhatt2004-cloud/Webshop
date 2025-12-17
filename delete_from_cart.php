6d14960a904d5de42d7c7827e23b61d21977616c
Divya <divyabhatt@gmail.com>
2025-12-17 17:07:11 +0530

    Update delete_from_cart.php

--- delete_from_cart.php
+++ delete_from_cart.php
@@ -1,6 +1,11 @@
 <?php
 include('./Connect.php');
 
+if(!$_SESSION['isLogin'])
+{
+    header('Location:Login_User.php');
+}
+
 $id = $_GET['id'];
 $cart_query = "DELETE FROM `cart` WHERE `id`= $id";
 $cart_result = mysqli_query($conn, $cart_query);
