97b08370ed6cfbb9c83873dd319109f27442b72f
Divya <divyabhatt@gmail.com>
2025-12-17 17:07:06 +0530

    Update Contact_Delete.php

--- Contact_Delete.php
+++ Contact_Delete.php
@@ -1,5 +1,11 @@
 <?php
 include "./Connect.php";
+
+if(!$_SESSION['isLogin'])
+{
+    header('Location:Login_User.php');
+}
+
 $id = $_GET["id"];
 $userquery = "DELETE FROM `contact_us` WHERE `id`= $id";
 
