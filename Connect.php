550786aff158de7eb2f1107e3b15ff55632d5342
Raghav joshi <joshiraghav282@gmail.com>
2025-12-19 10:49:44 +0530

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -1,9 +1,12 @@
 <?php
 $conn = mysqli_connect('localhost','root','','ecommerce');
-session_start();
-$_SESSION['isLogin'] =false;
-$_SESSION["user_type"] = 'user';
+
 if (!$conn) {
     echo "Connetion failed";
+}else{
+    session_start();
+    
+    $_SESSION['isLogin'] =false;
+    $_SESSION["user_type"] = 'user';
 }
 ?>
