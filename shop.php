9b0409cd60ae6aee16e99b27bc974f5f3cf26731
Divya <divyabhatt2004@gmail.com>
2025-12-29 17:16:37 +0530

    Update shop.php

--- shop.php
+++ shop.php
@@ -1,11 +1,17 @@
 <?php
 include('./header_base.php');
 
+$id = $_SESSION["user_id"];
+
 $product_query = "SELECT * FROM `product`";
 $product_result = mysqli_query($conn, $product_query);
 
 $category_query = "SELECT * FROM `categories`";
 $category_result = mysqli_query($conn, $category_query);
+
+$fav_Query = "SELECT * FROM `wishlist` WHERE `user_id`= '$id'";
+$fav_Result = mysqli_query($conn, $fav_Query);
+// $fav_rows = mysqli_fetch_assoc($fav_Result);
 ?>
 <section class=" pt-5 pb-5">
     <p id="demo"></p>
@@ -65,16 +71,33 @@ $category_result = mysqli_query($conn, $category_query);
                     </div>
                     <div class="row text-center">
                         <?php
-                        while ($product_rows = mysqli_fetch_assoc($product_result)) {
+                        // while($fav_rows = mysqli_fetch_assoc($fav_Result) ){                          
+                        while ($product_rows = mysqli_fetch_assoc($product_result)) {      
                         ?>
                             <div class="col-md-4 mb-5" id="product">
                                 <div class="card product-wap mb-4 rounded-0" id="productcard">
                                     <div class="position-relative">
-                                        <img src="./productimage/<?php echo $product_rows['image']; ?>" class="card-img rounded-0 img-fluid">
+                                        <img src="./productimage/<?php echo $product_rows['image']; ?>" class="card-img rounded-0 img-fluid">  
                                         <ul class="list-unstyled start-0 end-0" id="actionBtns">
                                             <li>
-                                                <a class="text-white btn btn-success opacity-75 p-2 border-0 mb-1 rounded">
-                                                    <i class="fa-regular fa-heart" id="heart"></i>
+                                                <?php 
+                                                // while($fav_rows = mysqli_fetch_assoc($fav_Result) ){
+                                                 ?>
+                                                <a href="add_to_wishlist.php?id=<?php echo $product_rows['id']; ?>" class="text-white btn btn-success opacity-75 p-2 border-0 mb-1 rounded">
+                                                <?php 
+                                                // $product_id = $product_rows['id'];
+                                                
+                                                // if ($product_rows['id'] == $fav_rows['product_id'] ){
+                                                  ?> 
+                                                <!-- <i class="fa-solid fa-heart" id="heart"></i> -->
+                                                <?php
+                                                // } else{
+                                                    ?>
+                                                <i class="fa-regular fa-heart" id="heart"></i>
+                                                <?php
+                                                // }
+                                            // }
+                                                ?>
                                                 </a>
                                             </li>
                                             <li>
@@ -89,6 +112,9 @@ $category_result = mysqli_query($conn, $category_query);
                                                 </a>
                                             </li>
                                         </ul>
+                                        <?php
+                                        // }
+                                        ?>
                                     </div>
                                     <div class="card-body p-2 bg-white w-100 h-100">
                                         <div class="row">
@@ -114,6 +140,7 @@ $category_result = mysqli_query($conn, $category_query);
                             </div>
                         <?php
                         }
+                    // }
                         ?>
                     </div>
                 </div>
