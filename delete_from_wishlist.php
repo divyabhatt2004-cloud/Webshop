808a4661a67ca27c9c4fd31b697de7067fe5b2ed
Divya <divyabhatt2004@gmail.com>
2026-01-05 13:08:08 +0530

    Update delete_from_wishlist.php

--- delete_from_wishlist.php
+++ delete_from_wishlist.php
@@ -1,11 +1,12 @@
 <?php
 include('./connect.php');
 $id = $_GET['id'];
-$fav_Query = "DELETE FROM `wishlist` WHERE `product_id`= $id";
+$user_id = $_SESSION['user_id'];
+$fav_Query = "DELETE FROM `wishlist` WHERE `product_id`= '$id' AND `user_id`= '$user_id'";
 $fav_result = mysqli_query($conn, $fav_Query);
 
 if ($fav_result) {
-    if($_SERVER['HTTP_REFERER'] == "http://localhost/webshop/shop.php"){
+    if(basename($_SERVER['HTTP_REFERER']) == "shop.php"){
     header("Location:./shop.php");
     }else{
     header("Location:./wishlist.php");
